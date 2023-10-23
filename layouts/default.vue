379fe7274a57bf9d5d41d83a3e08d84fd6c11e09
springfil <st.kenzhaev@gmail.com>
2023-10-23 21:54:09 +0300

    added tailwind and played around with styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,23 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">Nuxt Shop</NuxtLink>
-                <ul>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <NuxtLink to="/" class="font-bold">Nuxt Shop</NuxtLink>
+                <ul class="flex gap-4">
                     <li><NuxtLink to="/">Home</NuxtLink></li>
                     <li><NuxtLink to="/about">About</NuxtLink></li>
-                    <li><NuxtLink to="/products">Products</NuxtLink></li>
+                    <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
                 </ul>
             </nav>
         </header>
-        <div>
-            <slot/>
+        <div class="container mx-auto p-4">
+            <slot />
         </div>
     </div>
 </template>
 
 <style scoped>
-  .router-link-exact-active {
+.router-link-exact-active {
     color: #12b488;
-  }
+}
 </style>
