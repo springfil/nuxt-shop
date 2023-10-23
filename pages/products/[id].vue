9b03be17c3b99f848eb87c70bdb60dd33383d168
springfil <st.kenzhaev@gmail.com>
2023-10-23 20:30:21 +0300

    init nuxt-shop

---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Products details for {{ id }}</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            aperiam fugiat at dolor rerum facilis, vitae, consectetur debitis
+            illum molestias explicabo, neque labore iure? Necessitatibus ducimus
+            illum aspernatur cupiditate tenetur.
+        </p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+
+</script>
+
+<style scoped></style>
